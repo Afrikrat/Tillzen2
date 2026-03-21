a3c427445f42d1e1ccf86acf89b52682d2ffdda4
Akwasi Pixels <afrikrat@gmail.com>
2026-03-21 04:56:27 +0100

    fix: convert MainActivity to Java for better build resilience

---
+++ android/app/src/main/java/com/tillzen/tillzen/MainActivity.java
@@ -0,0 +1,6 @@
+package com.tillzen.tillzen;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
--- android/app/src/main/kotlin/com/tillzen/tillzen/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.tillzen.tillzen
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
